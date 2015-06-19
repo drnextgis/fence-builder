caa67ef405131e9932a1e8758608cadb6aea77be
drnextgis <rykovd@gmail.com>
2015-06-19 21:57:21 +0600

    Move to gh-pages

---
+++ Fence.js
@@ -0,0 +1,379 @@
+function Fence(table, close, split, scale) {
+    this.initialize(table, close, split, scale);
+};
+
+
+Fence.prototype.initialize = function(table, close, split, scale) {
+    this.split  = split;
+    this.scale  = scale;
+    this.edges  = [];
+
+    // Непрерывные сегменты, сгруппированы по сторонам
+    this.segments = [];
+
+    // Калитки, ворота, пропуски
+    // сгруппированы по сторонам
+    this.entrances = [];
+
+    // Столбы
+    this.columns = [];
+
+    // Участки между столбами
+    this.parts = [];
+
+    this.source = this.parseTable(table);
+    this.source.close = close;
+
+    this.buildEdges();
+    this.buildEntrances();
+    this.buildSegments();
+    this.buildColumns();
+};
+
+
+Fence.prototype.buildEdges = function() {
+    var x1, x2, y1, y2, a, l, f, dx, dy;
+    var fence = this;
+
+    $.each(this.source.sides, function(i, side) {
+        l = side.length;
+        a = side.angle;
+        f = side.frequency;
+        if (i == 0) {
+            x1 = 0;
+            y1 = 0;
+            x2 = x1 + l*Math.cos(Math.PI*a/180)*fence.scale;
+            y2 = y1 + l*Math.sin(Math.PI*a/180)*fence.scale;
+        } else {
+            a += fence.edges[i-1].angle;
+            x1 = fence.edges[i-1].x2;
+            y1 = fence.edges[i-1].y2;
+            x2 = x1 + l*Math.cos(Math.PI*a/180)*fence.scale;
+            y2 = y1 + l*Math.sin(Math.PI*a/180)*fence.scale;
+        }
+        fence.edges.push({
+            x1: x1,
+            y1: y1,
+            x2: x2,
+            y2: y2,
+            angle: a,
+            frequency: f,
+            length: l
+        });
+        if ((i > 0) && (i == (fence.source.sides.length -1)) && fence.source.close) {
+            x1 = x2;
+            y1 = y2;
+            x2 = fence.edges[0].x1;
+            y2 = fence.edges[0].y1;
+            dx = x2 - x1;
+            dy = y2 - y1;
+            a = Math.atan2(dy, dx)*(180/Math.PI);
+            l = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))/fence.scale;
+
+            fence.edges.push({
+                x1: x1,
+                y1: y1,
+                x2: x2,
+                y2: y2,
+                angle: a,
+                frequency: f,
+                length: l
+            });
+        }
+    });
+};
+
+
+Fence.prototype.buildEntrances = function() {
+    var x1, x2, y1, y2;
+    var fence = this;
+
+    $.each(this.source.sides, function(i, side) {
+        if (side.entrances.length == 0) {
+            fence.entrances.push([]);
+        } else {
+            var edge = fence.edges[i];
+            var entrances = [];
+            $.each(side.entrances, function(j, entrance) {
+                x1 = edge.x1 + entrance.offset*Math.cos(Math.PI*edge.angle/180)*fence.scale;
+                y1 = edge.y1 + entrance.offset*Math.sin(Math.PI*edge.angle/180)*fence.scale;
+                x2 = x1 + entrance.length*Math.cos(Math.PI*edge.angle/180)*fence.scale;
+                y2 = y1 + entrance.length*Math.sin(Math.PI*edge.angle/180)*fence.scale;
+                entrances.push({
+                    x1: x1,
+                    y1: y1,
+                    x2: x2,
+                    y2: y2,
+                    angle: edge.angle,
+                    type: entrance.type,
+                    length: entrance.length
+                });
+            });
+            fence.entrances.push(entrances);
+        }
+    });
+    // Делаем предположение, что на замыкающей стороне
+    // нет калиток, ворот или препятствий
+    if (fence.source.close) {
+        fence.entrances.push([]);
+    }
+};
+
+
+Fence.prototype.buildSegments = function() {
+    var coords, fence = this;
+
+    $.each(this.edges, function(i, edge) {
+        var segment, segments = [];
+
+
+        var segmentXStart = edge.x1,
+            segmentYStart = edge.y1,
+            segmentXEnd = edge.x1,
+            segmentYEnd = edge.y1;
+
+        $.each(fence.entrances[i], function(j, entrance) {
+            segmentXEnd = entrance.x1;
+            segmentYEnd = entrance.y1;
+
+            if (!((segmentXStart == segmentXEnd) && (segmentYStart == segmentYEnd))) {
+                segments.push({
+                    x1: segmentXStart,
+                    y1: segmentYStart,
+                    x2: segmentXEnd,
+                    y2: segmentYEnd
+                });
+            }
+
+            segmentXStart = entrance.x2;
+            segmentYStart = entrance.y2;
+        });
+
+        segmentXEnd = edge.x2;
+        segmentYEnd = edge.y2;
+
+        if (!((segmentXStart == segmentXEnd) && (segmentYStart == segmentYEnd))) {
+            segments.push({
+                x1: segmentXStart,
+                y1: segmentYStart,
+                x2: segmentXEnd,
+                y2: segmentYEnd
+            });
+        }
+
+        fence.segments.push(segments);
+    });
+};
+
+
+Fence.prototype.buildColumns = function() {
+    var ptJSTS, segmentJSTS, partJSTS, partJSTSLength, segmentJSTSLength, pt,
+        columnJSTS = new jsts.geom.MultiPoint(),
+        columnRegularJSTS = new jsts.geom.MultiPoint(),
+        columnIrregularJSTS = new jsts.geom.MultiPoint(),
+        part, lastColumn,
+        fence = this;
+
+    $.each(this.entrances, function(i, entranceGroup) {
+        $.each(entranceGroup, function(j, entrance) {
+            // Столбы для калиток и ворот
+            if (entrance.type !== "obstacle") {
+                $.each([
+                    new jsts.geom.Point(new jsts.geom.Coordinate(
+                        entrance.x1,
+                        entrance.y1
+                    )),
+                    new jsts.geom.Point(new jsts.geom.Coordinate(
+                        entrance.x2,
+                        entrance.y2
+                    ))
+                ], function(k, pt) {
+                    if (!columnJSTS.contains(pt)) {
+                        columnJSTS = columnJSTS.union(pt);
+                        columnIrregularJSTS = columnIrregularJSTS.union(pt)
+                    }
+                });
+            }
+        });
+    });
+
+
+    // Столбы для непрерывных сегментов
+    $.each(this.edges, function(i, edge) {
+        $.each(fence.segments[i], function(j, segment) {
+            segmentJSTS = new jsts.geom.LineSegment(
+                new jsts.geom.Coordinate(segment.x1, segment.y1),
+                new jsts.geom.Coordinate(segment.x2, segment.y2)
+            );
+            lastColumn = {
+                x: segment.x1,
+                y: segment.y1
+            };
+
+            segmentJSTSLength = +(segmentJSTS.getLength().toFixed(1));
+            for (var offset = 0; offset <= (segmentJSTS.getLength()/fence.scale); offset += edge.frequency) {
+                ptJSTS = new jsts.geom.Point(new jsts.geom.Coordinate(
+                    segment.x1 + offset*Math.cos(Math.PI*edge.angle/180)*fence.scale,
+                    segment.y1 + offset*Math.sin(Math.PI*edge.angle/180)*fence.scale
+                ));
+
+                if (!columnJSTS.contains(ptJSTS)) {
+                    columnJSTS = columnJSTS.union(ptJSTS);
+                    columnRegularJSTS = columnRegularJSTS.union(ptJSTS);
+                }
+
+                if (offset > 0) {
+                    part = {
+                        x1: lastColumn.x,
+                        y1: lastColumn.y,
+                        x2: ptJSTS.getX(),
+                        y2: ptJSTS.getY(),
+                        angle: edge.angle,
+                        length: edge.frequency
+                    };
+                    fence.parts.push(part)
+                    lastColumn.x = part.x2;
+                    lastColumn.y = part.y2;
+                }
+
+                // Предпоследний столб ставим в середину
+                if (fence.split){
+                    partJSTS = new jsts.geom.LineSegment(
+                        new jsts.geom.Coordinate(ptJSTS.getX(), ptJSTS.getY()),
+                        new jsts.geom.Coordinate(segment.x2, segment.y2)
+                    );
+
+                    partJSTSLength = +(partJSTS.getLength().toFixed(1));
+                    if (
+                        (partJSTSLength < edge.frequency*fence.scale*2) &&
+                        (partJSTSLength > edge.frequency*fence.scale)
+                    ) {
+                        pt = new jsts.geom.Point(new jsts.geom.Coordinate(
+                            ptJSTS.getX() + (partJSTSLength/2)*Math.cos(Math.PI*edge.angle/180),
+                            ptJSTS.getY() + (partJSTSLength/2)*Math.sin(Math.PI*edge.angle/180)
+                        ));
+                        if (!columnJSTS.contains(pt)) {
+                            columnJSTS = columnJSTS.union(pt);
+                            columnRegularJSTS = columnRegularJSTS.union(pt);
+                        }
+
+                        part = {
+                            x1: lastColumn.x,
+                            y1: lastColumn.y,
+                            x2: pt.getX(),
+                            y2: pt.getY(),
+                            angle: edge.angle,
+                            length: (partJSTSLength/2)/fence.scale
+                        }
+                        fence.parts.push(part);
+                        lastColumn.x = part.x2;
+                        lastColumn.y = part.y2;
+                        break;
+                    }
+                }
+            }
+
+            // Последний столб
+            pt = new jsts.geom.Point(new jsts.geom.Coordinate(
+                segment.x2,
+                segment.y2
+            ));
+            if (!columnJSTS.contains(pt)) {
+                columnJSTS = columnJSTS.union(pt);
+                columnRegularJSTS = columnRegularJSTS.union(pt);
+            }
+
+            var dx = segment.x2 - lastColumn.x;
+            var dy = segment.y2 - lastColumn.y;
+            var length = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))/fence.scale;
+            if (length !== 0) {
+                part = {
+                    x1: lastColumn.x,
+                    y1: lastColumn.y,
+                    x2: segment.x2,
+                    y2: segment.y2,
+                    angle: edge.angle,
+                    length: length
+                };
+                fence.parts.push(part);
+            }
+        });
+    });
+
+    for (var gid = 0; gid < columnJSTS.getNumGeometries(); gid += 1) {
+        var geometry = columnJSTS.getGeometryN(gid),
+            coords = geometry.getCoordinates(),
+            columnType = (columnIrregularJSTS.contains(geometry)) ? "irregular" : "regular";
+
+        fence.columns.push({
+            x: coords[0].x,
+            y: coords[0].y,
+            type: columnType
+        });
+    }
+};
+
+
+Fence.prototype.render = function(renderer) {
+    renderer.render(this);
+};
+
+
+Fence.prototype.parseTable = function(table) {
+    var side, length, frequency, angle,
+        gateLength, wicketLength, obstacleLength,
+        gateOffset, wicketOffset, obstacleOffset,
+        source = {sides: []};
+
+    $("#" + table + " tbody tr").each(function(i) {
+        length         = parseFloat($(this).find(".length").text());
+        frequency      = parseFloat($(this).find(".frequency").text());
+        angle          = parseFloat($(this).find(".angle").text());
+        gateLength     = parseFloat($(this).find(".gate").text());
+        wicketLength   = parseFloat($(this).find(".wicket").text());
+        obstacleLength = parseFloat($(this).find(".obstacle").text());
+        gateOffset     = parseFloat($(this).find(".gate-offset").text());
+        wicketOffset   = parseFloat($(this).find(".wicket-offset").text());
+        obstacleOffset = parseFloat($(this).find(".obstacle-offset").text());
+
+        side = {
+            length: length,
+            frequency: frequency,
+            angle: angle,
+            entrances: []
+        };
+
+        if (gateLength > 0) {
+            side.entrances.push({
+                length: gateLength,
+                offset: gateOffset,
+                type:   "gate"
+            });
+        }
+
+        if (wicketLength > 0) {
+            side.entrances.push({
+                length: wicketLength,
+                offset: wicketOffset,
+                type: "wicket"
+            });
+        }
+
+        if (obstacleLength > 0) {
+            side.entrances.push({
+                length: obstacleLength,
+                offset: obstacleOffset,
+                type: "obstacle"
+            });
+        }
+
+        // Сортируем ворота, калитки и препятствия в порядке
+        // удалённости от начала стороны забора
+        side.entrances.sort(function(a, b) {
+            return a.offset - b.offset;
+        });
+
+        source.sides.push(side);
+    });
+    return source;
+};
---
+++ Renderer.js
@@ -0,0 +1,164 @@
+function Renderer(canvas) {
+    this.canvas = new fabric.StaticCanvas(canvas);
+    this.group  = new fabric.Group();
+};
+
+
+Renderer.prototype.render = function(fence) {
+    this.renderSegments(fence);
+    this.renderEntrances(fence);
+    this.renderColumns(fence);
+
+    this.renderEntranceLabels(fence);
+    this.renderSegmentLabels(fence);
+
+    this.canvas.add(this.group);
+    this.canvas.centerObject(this.group);
+
+};
+
+
+Renderer.prototype.renderSegments = function(fence) {
+    var obj,
+        renderer = this;
+
+    $.each(fence.edges, function(i, edge) {
+        $.each(fence.segments[i], function(j, segment) {
+            obj = new fabric.Line([
+                segment.x1,
+                segment.y1,
+                segment.x2,
+                segment.y2
+            ], {
+                stroke: "black",
+                strokeWidth: 3,
+                hasBorders: false
+            });
+            renderer.group.addWithUpdate(obj);
+        });
+    });
+};
+
+
+Renderer.prototype.renderEntrances = function(fence) {
+    var obj,
+        renderer = this;
+
+    $.each(fence.edges, function(i, edge) {
+        $.each(fence.entrances[i], function(j, entrance) {
+            obj = new fabric.Line([
+                entrance.x1,
+                entrance.y1,
+                entrance.x2,
+                entrance.y2
+            ], {
+                stroke: "red",
+                strokeWidth: 1,
+                strokeDashArray: [5, 5],
+                hasBorders: false
+            });
+            renderer.group.addWithUpdate(obj);
+        });
+    });
+};
+
+
+Renderer.prototype.renderColumns = function(fence) {
+    var obj,
+        renderer = this;
+
+    $.each(fence.columns, function(i, column) {
+        obj = new fabric.Circle({
+            top: column.y,
+            left: column.x,
+            radius: 5,
+            stroke: "black",
+            strokeWidth: 1,
+            fill: (column.type == "regular") ? "white" : "black",
+            originY: "center",
+            originX: "center",
+            hasBorders: false
+        });
+        renderer.group.addWithUpdate(obj);
+    });
+};
+
+
+Renderer.prototype.renderSegmentLabels = function(fence) {
+    var edgeLength, el, partSize, ps,
+        renderer = this;
+
+    $.each(fence.edges, function(i, edge) {
+        edgeLength = "L " + (i + 1) + " = " + edge.length.toFixed(1);
+        el = new fabric.Text(edgeLength, {
+            left: (edge.x1 + edge.x2)/2,
+            top:  (edge.y1 + edge.y2)/2,
+            angle: edge.angle,
+            fontSize: 12,
+            lineHeight: 5,
+            originY: "botom",
+            originX: "center",
+            fontFamily: "arial",
+            fontWeight: "bold",
+            textDecoration: "underline",
+            hasBorders: false
+        });
+        renderer.group.addWithUpdate(el);
+    });
+
+    $.each(fence.parts, function(i, part) {
+        partSize = part.length.toFixed(1);
+        ps = new fabric.Text(partSize, {
+            left: (part.x1 + part.x2)/2,
+            top:  (part.y1 + part.y2)/2,
+            angle: part.angle,
+            fontSize: 12,
+            fontFamily: "arial",
+            lineHeight: 2.5,
+            originY: "botom",
+            originX: "center",
+            hasBorders: false
+        });
+        renderer.group.addWithUpdate(ps);
+    });
+};
+
+
+Renderer.prototype.renderEntranceLabels = function(fence) {
+    var entranceType, entranceSize, t, s,
+        renderer = this;
+
+    $.each(fence.edges, function(i, edge) {
+        $.each(fence.entrances[i], function(j, entrance) {
+            entranceSize = entrance.length.toFixed(1);
+            entranceType = (
+                (entrance.type == "gate") ? "В" :
+                (entrance.type == "wicket") ? "К" : "П"
+            );
+
+            t = new fabric.Text(entranceType, {
+                left: (entrance.x1 + entrance.x2)/2,
+                top:  (entrance.y1 + entrance.y2)/2,
+                originY: "top",
+                originX: "center",
+                angle: entrance.angle,
+                fontSize: 12,
+                fontFamily: "arial",
+                hasBorders: false
+            });
+            renderer.group.addWithUpdate(t);
+
+            s = new fabric.Text(entranceSize, {
+                left: (entrance.x1 + entrance.x2)/2,
+                top:  (entrance.y1 + entrance.y2)/2,
+                originX: "center",
+                originY: "bottom",
+                angle: entrance.angle,
+                fontSize: 12,
+                fontFamily: "arial",
+                hasBorders: false
+            });
+            renderer.group.addWithUpdate(s);
+        });
+    });
+};
